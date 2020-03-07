1640cf7d859c7f54ae232e9e813727470a0f5211
Anastasiya <anastasiya.tarazevich22@gmail.com>
2020-03-07 19:14:52 +0300

    forest

---
+++ Assets/DayNight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour
+{
+
+
+    public GameObject sun;
+    
+    public float a = 0;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        a = sun.transform.rotation.x;
+        sun.transform.Rotate(Vector3.right *   Time.deltaTime/20);
+
+        if (a == 1)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+        
+    }
+}
